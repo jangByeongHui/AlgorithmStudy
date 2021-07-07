4bf81e9fea05e84d3c41b35f26e7887cfe36906f
JangByeongHui <jmkabc31@ajou.ac.kr>
2021-07-07 20:22:49 +0900

    1388문제 해결

--- 1388.py
+++ 1388.py
@@ -1,6 +1,36 @@
 
+n,m=input().split()
+
+n=int(n)
+m=int(m)
+stack=list()
+count=0
+visit=[[0 for _ in range(m)] for _ in range(n)]
+map=[]
+#맵 입력
+for i in range(n):
+    map.append(list(input()))
+#같은 모양인데 방문하지 않았다면 방문으로 표시 최초 발견된 건 +1 추가
+for i in range(n):
+    for j in range(m):
+        if map[i][j]=='-' and visit[i][j]==0:
+            count=count+1
+            visit[i][j]=1
+            k=j+1
+            while k<m and map[i][k]=='-' :
+                visit[i][k]=1
+                k=k+1
+        if map[i][j]=='|' and visit[i][j]==0:
+            count=count+1
+            visit[i][j]=1
+            k=i+1
+            while k<n and map[k][j]=='|' :
+                visit[k][j]=1
+                k=k+1
+
+print(count)
 
 
 
 
-if __name__=="__main__":
+
