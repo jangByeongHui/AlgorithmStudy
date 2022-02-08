83a229e4a47dee18ccf616e7460c0fc1f0268685
장병희 <jmkabc31@ajou.ac.kr>
2022-02-08 12:52:45 +0900

    solved 17626.py

--- 17626.py
+++ 17626.py
@@ -1,20 +1,17 @@
 import sys
 import math
 
-def cal(num):
-    count = 0
-    while num >= 1:
-        count += 1
-        num -= math.pow(int(math.sqrt(num)), 2)
-    return count
-
 num=int(sys.stdin.readline())
 
-dp=[0 for _ in range(num+1)]
+dp=[4 for _ in range(num+1)]
+dp[0]=0
 dp[1]=1
 
 for i in range(2,num+1):
-    dp[i]=min(dp[i-1]+1,cal(i))
+    for j in range(int(math.sqrt(i)+1)):
+        dp[i]=min(dp[i],dp[i-j**2])
+    dp[i]+=1
+
 
 print(dp[num])
 
