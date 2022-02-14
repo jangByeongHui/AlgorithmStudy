70c0131c87ee618d6bf243fc40a435698f5e6a84
장병희 <jmkabc31@ajou.ac.kr>
2022-02-14 13:27:06 +0900

    solved 11659.py

--- 11659.py
+++ 11659.py
@@ -3,14 +3,11 @@ import sys
 N,M =map(int,sys.stdin.readline().split())
 
 num= list(map(int,sys.stdin.readline().split()))
+dp = [0 for _ in range(len(num)+1)]
 
-
-dp=[[0 for _ in range(len(num))] for _ in range(len(num))]
-
-for i in range(N):
-    for j in range(i,N):
-        dp[i][j]=num[j]+dp[i][j-1]
+for i in range(1,len(num)+1):
+    dp[i]=dp[i-1]+num[i-1]
 
 for _ in range(M):
     first, last = map(int,sys.stdin.readline().split())
-    print(dp[first-1][last-1])
+    print(dp[last]-dp[first-1])
