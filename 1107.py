5e64f5953cca6eb0f69a65d45d399b1262fcff19
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-02-20 01:04:21 +0900

    37%

--- 1107.py
+++ 1107.py
@@ -4,7 +4,7 @@ Target = sys.stdin.readline().rstrip()
 Target_int =int(Target)
 Target_len=len(Target)
 broken_num=int(sys.stdin.readline())
-Min = Target_int
+Min = 99999999
 Min_Value=0
 Min_count=0
 
@@ -13,26 +13,28 @@ if broken_num!=0:
 else:
     broken_Buttons = []
 
-available_buttons=[i for i in range(9,-1,-1)]
+available_buttons=[i for i in range(10)]
 now_ch = 100
 
 for removeValue in broken_Buttons:
-    if removeValue == '0':
-        continue
     available_buttons.remove(removeValue)
 
 def dfs(N, num, Target_len):
     global Min, Min_count,available_buttons,Min_Value
-    if N == Target_len:
+    if N == Target_len+1:
         if Min > abs(int(num)-Target_int):
             Min_Value=int(num)
             Min = abs(int(num) - Target_int)
-            if Min_Value==0:
-                Min_count=0
-            else:
-                Min_count=len(str(Min_Value))
+            Min_count=len(str(Min_Value))
         return
     else:
+        try:
+            if Min > abs(int(num)-Target_int):
+                Min_Value=int(num)
+                Min = abs(int(num) - Target_int)
+                Min_count=len(str(Min_Value))
+        except:
+            pass
         for button in available_buttons:
             dfs(N+1,num+str(button),Target_len)
 
@@ -40,25 +42,13 @@ def dfs(N, num, Target_len):
 if Target_int == now_ch:
     print(0)
 
+elif broken_num == 10:
+    print(abs(Target_int-100))
 elif abs(now_ch-Target_int)<=len(Target):
     print(abs(now_ch-Target_int))
 
 else:
-    count = 0
-    if Target_len<2:
-        dfs(0, "", Target_len+1)
-    else:
-        dfs(0,"",Target_len)
 
-    print(Min_count+abs(Min_Value-Target_int))
-
-
-
-
-
-
-
-
-
-
-
+    dfs(0,"",Target_len)
+    # print(f'Min_count : {Min_count} Min_Value : {Min_Value} Targe_int : {Target_int}')
+    print(min(Min_count+abs(Min_Value-Target_int),abs(now_ch-Target_int)))
