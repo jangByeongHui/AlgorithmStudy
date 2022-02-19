6c3fc71d5fdcf5d1fcae3e5c9b3e3ab328816ed4
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-02-20 00:19:15 +0900

    saved 1107

--- 1107.py
+++ 1107.py
@@ -2,46 +2,63 @@ import sys
 
 Target = sys.stdin.readline().rstrip()
 Target_int =int(Target)
+Target_len=len(Target)
 broken_num=int(sys.stdin.readline())
+Min = Target_int
+Min_Value=0
+Min_count=0
 
 if broken_num!=0:
     broken_Buttons=list(map(int,sys.stdin.readline().rstrip().split()))
 else:
     broken_Buttons = []
 
-
 available_buttons=[i for i in range(9,-1,-1)]
 now_ch = 100
 
 for removeValue in broken_Buttons:
+    if removeValue == '0':
+        continue
     available_buttons.remove(removeValue)
+
+def dfs(N, num, Target_len):
+    global Min, Min_count,available_buttons,Min_Value
+    if N == Target_len:
+        if Min > abs(int(num)-Target_int):
+            Min_Value=int(num)
+            Min = abs(int(num) - Target_int)
+            if Min_Value==0:
+                Min_count=0
+            else:
+                Min_count=len(str(Min_Value))
+        return
+    else:
+        for button in available_buttons:
+            dfs(N+1,num+str(button),Target_len)
+
 
 if Target_int == now_ch:
     print(0)
+
 elif abs(now_ch-Target_int)<=len(Target):
     print(abs(now_ch-Target_int))
+
 else:
     count = 0
-    cal_value=""
-    for i in range(len(Target)):
-        Min=9
-        Max=-1
-        MinMax_str="9"
-        for available_button in available_buttons:
-            if i==0 and abs(int(Target[i]) - available_button)<Min:
-                Min = abs(int(Target[i]) - available_button)
-                MinMax_str=str(available_button)
-            elif i!=0 and int(Target[0]) in available_buttons and abs(int(Target[i]) - available_button)<Min:
-                Min = abs(int(Target[i]) - available_button)
-                MinMax_str = str(available_button)
-            elif i!=0 and int(Target[0]) not in available_buttons and abs(int(Target[i]) - available_button)>Max:
-                Max = abs(int(Target[i]) - available_button)
-                MinMax_str = str(available_button)
-        cal_value += MinMax_str
-        count += 1
-    count += abs(int(cal_value)-Target_int)
+    if Target_len<2:
+        dfs(0, "", Target_len+1)
+    else:
+        dfs(0,"",Target_len)
 
-    print(count)
+    print(Min_count+abs(Min_Value-Target_int))
+
+
+
+
+
+
+
+
 
 
 
