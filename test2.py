cd0528805e2dfd1c7bcba8f49ec01a0eadde3da9
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-08-30 18:16:54 +0900

    roll dice 2

--- test2.py
+++ test2.py
@@ -34,64 +34,49 @@ def rolling_dice(dice, dir):
 
 
 def calculate_point(x, y, board_dir):
-    temp_dice = copy.deepcopy(dice)
 
     target_value = board[x][y]
-    result = -sys.maxsize
+    result = 0
     visited = [[False for _ in range(M)] for _ in range(N)]
-    queue = deque([(x, y, 1)])
-    temp_board_dir = board_dir
-    print(target_value)
+    queue = deque([(x, y)])
+
+    point_direction = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
     while queue:
 
-        tx, ty, tcount = queue.popleft()
-        result = max(result,tcount)
-        print("########################\n")
-        print(temp_dice[3][1], temp_board_dir)
-        for row in visited:
-            print(row)
-        print("########################\n")
-
+        tx, ty = queue.popleft()
         if visited[tx][ty] == False:
-            rolling_dice(temp_dice, temp_board_dir)
-            dx, dy = direction[temp_board_dir]
-
-            if 0 <= tx + dx < N and 0 <= ty + dy < M:
-                if board[tx+dx][ty+dy] == target_value:
-                    queue.append((tx+dx,ty+dy,tcount+1))
-                else:
-                    queue.append((tx + dx, ty + dy, 0))
-
-                if temp_dice[3][1] > board[tx+dx][ty+dy]:
-                    temp_board_dir = (temp_board_dir + 1) % 4  # 다음 진행방향 90도 시계 방향으로 변환
-                elif temp_dice[3][1] < board[tx+dx][ty+dy]:
-                    temp_board_dir = temp_board_dir - 1 if (temp_board_dir - 1 < 0) else 0
-            else:
-                if board[tx-dx][ty-dy] == target_value:
-                    queue.append((tx-dx,ty-dy,tcount+1))
-                else:
-                    queue.append((tx - dx, ty - dy, 0))
-                temp_board_dir = (temp_board_dir + 2) % 4  # 반대 방향으로 변환
+            result += 1
+            for dx, dy in point_direction:
+                if 0 <= tx + dx < N and 0 <= ty + dy < M:
+                    if board[tx+dx][ty+dy] == target_value:
+                        queue.append((tx+dx,ty+dy))
 
             visited[tx][ty] = True #방문한 위치는 재방문 안함
+        print("#######################")
+        print(f'target_value :{target_value} tx: {tx} ty: {ty} tcount: {result} board_dir: {board_dir}')
+        for row in visited:
+            print(row)
+        print("#######################\n")
     return result*target_value
 
 
 board_dir = 0
 x, y = 0, 0
 point = 0
-for _ in range(K):
-    rolling_dice(dice, board_dir)
-    print("###########Rolling Dice############")
-    for row in dice:
-        print(row)
-    print("##################################\n")
+for index in range(K):
+
 
     dx, dy = direction[board_dir]
 
     x += dx
     y += dy
-
+    rolling_dice(dice, board_dir)
+    print(f"########### {index + 1} Rolling Dice############")
+    for row in dice:
+        print(row)
+    print("##################################\n")
+    print(f"dice[3][1]: {dice[3][1]},board_dir: {board_dir}")
     if 0 <= x < N and 0 <= y < N:
         if dice[3][1] > board[x][y]:
             board_dir = (board_dir + 1) % 4  # 다음 진행방향 90도 시계 방향으로 변환
@@ -103,7 +88,7 @@ for _ in range(K):
             point += calculate_point(x, y, board_dir)
     else:
         x -= 2 * dx
-        y -= 2 * dx
+        y -= 2 * dy
         board_dir = (board_dir + 2) % 4  # 반대 방향으로 변환
         point += calculate_point(x, y, board_dir)
 
