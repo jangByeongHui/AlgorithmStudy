ca3d56c4c9ec6cc9653875d496999609f076c7be
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-10-18 18:01:00 +0900

    checkpoint SOFTEER 자율주행 교차로

--- softeer/cross_walk.py
+++ softeer/cross_walk.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import deque, defaultdict
 
 N = int(input())
 
@@ -10,34 +10,61 @@ for id in range(N):
     time, type = input().split()
     time = int(time)
     now_time = min(now_time,time)
-    cars.append({"start_time":time,"type":type,"id":id,"cross":False})
+    cars.append({"start_time":time,"end_time":-1,"type":type,"id":id,"cross":False})
 
 
 result = []
 right_car = {"A":"D","B":"A","C":"B","D":"C"}
 
-while cars:
+while True:
 
-    car = cars.popleft()
-
-    # 이미 지나간 차량
-    if car["cross"]:
-        continue
-
-    if car["start_time"] > now_time: # 아직 교차로에 진입할 때가 아님
-        continue
-
-    # 차량 위치 파악
-    for next_car in cars:
-        if next_car["cross"] or next_car["start_time"] > now_time: # 지나간 차량과 교차로에 진입하지 않은 차량은 무시
-            continue
-        # 오른쪽에 차량이 있음 다음에 통과
-        if next_car["type"] == right_car[car["type"]]:
-            cars.append(car)
+    # 모든 차량이 교차로를 통과하였다면 종료
+    for car in cars:
+        if not car["cross"]:
             break
-    else: # 어떠한 문제가 없음으로 교차로 통행
-        result.append((car["id"],now_time))
+    else:
+        break
 
-    now_time += 1 # 매번 시간 증가
+    candidate_cars = deque()
 
-print(result)
+    # 지나갈 수 있는 차량 선별
+    pass_car = defaultdict(bool)
+    for car in cars:
+        if car["start_time"] > now_time or car["cross"]: # 시간이 되지 않았거나 지나간 차량은 제외
+            continue
+        if pass_car[car["type"]]:
+            continue
+        pass_car[car["type"]] = True # 한 교차로에서 한번만 지나 다닐 수 있음으로 이미 선택한 교차로는 pass
+        candidate_cars.append(car)
+
+    # 교착 상태에 도달하는지 판단
+    A, B, C, D = False, False, False, False
+
+    for candidate in candidate_cars:
+        if candidate["type"] == "A":
+            A = True
+        elif candidate["type"] == "B":
+            B = True
+        elif candidate["type"] == "C":
+            C = True
+        else:
+            D = True
+
+    if A and B and C and D: # 교착 상태에 도달하였음으로 종료
+        break
+
+    for car_index,car in enumerate(candidate_cars): # 최대 비교를 4*4만 하게 됨
+
+        for next_car_index,next_car in enumerate(candidate_cars):
+            if car_index == next_car_index: # 자기 자신 비교는 skip
+                continue
+            if right_car[car["type"]] == next_car["type"]: # 오른쪽에 차량이 위치하고 있으면 지나 갈 수 없음
+                break
+        else: # 지나갈 수 있음
+            car["end_time"] = now_time
+            car["cross"] = True
+
+    now_time += 1
+
+for car in cars:
+    print(car["end_time"])
