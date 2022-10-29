6f329f6e1b703650feef0087a66ae82248dbf427
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-10-29 21:57:50 +0900

    17299 solved

---
+++ DoIt_Python_Study/5weeks/practice/17299.py
@@ -0,0 +1,17 @@
+from collections import Counter
+from sys import stdin
+
+n = int(stdin.readline())
+nums = list(map(int, stdin.readline().split()))
+
+nums_count = Counter(nums)
+
+answer = [-1] * n
+stack = []
+
+for i in range(n):
+    while stack and nums_count[nums[stack[-1]]] < nums_count[nums[i]]:
+            answer[stack.pop()] = nums[i]
+    stack.append(i)
+
+print(*answer)
