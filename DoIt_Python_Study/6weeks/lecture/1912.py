53cf4efa4e8efa727ec0ce8e72f1232460149b3d
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-11-06 17:36:16 +0900

    solved 1912

---
+++ DoIt_Python_Study/6weeks/lecture/1912.py
@@ -0,0 +1,9 @@
+N = int(input())
+elements = list(map(int, input().split(' ')))
+
+for i in range(1, N):
+    elements[i] = max(elements[i], elements[i] + elements[i - 1])
+
+print(max(elements))
+
+# 참고 : https://data-marketing-bk.tistory.com/52
