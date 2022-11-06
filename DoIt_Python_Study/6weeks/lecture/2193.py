639790bb718553f67f63d16c58cb999ae9e1e694
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-11-06 17:28:30 +0900

    solved 2193

---
+++ DoIt_Python_Study/6weeks/lecture/2193.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+# n 자리 수 만큼 dp 테이블 생성 => n+1
+dp = [0] * (N + 1)
+dp[1] = 1
+
+# dp 점화식
+# 경우의 수를 따져보면 피보나치 수열과 비슷한 형태를 띄고 있음
+for i in range(2, N+1):
+    dp[i] = dp[i-2] + dp[i-1]
+
+print(dp[N])
+
+# 참고 : https://cijbest.tistory.com/17
