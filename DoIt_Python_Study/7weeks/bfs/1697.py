b4b6f7c8ed2503e189af4b814aa769c453fe83c2
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-11-22 17:22:22 +0900

    solved 1697

---
+++ DoIt_Python_Study/7weeks/bfs/1697.py
@@ -0,0 +1,30 @@
+from collections import deque,defaultdict
+
+N, K = map(int,input().split())
+
+queue = deque([(N,0)])
+visited = defaultdict(bool)
+
+while queue:
+
+    subin_n, subin_move = queue.popleft()
+
+    if subin_n == K:
+        print(subin_move)
+        break
+
+    if visited[subin_n]:
+        continue
+
+    visited[subin_n] = True
+
+    if 0 <= subin_n+1 <= 100000:
+        queue.append((subin_n+1,subin_move+1))
+
+    if 0 <= subin_n-1 <= 100000:
+        queue.append((subin_n-1, subin_move + 1))
+
+    if 0 <= 2*subin_n <= 100000:
+        queue.append((2*subin_n, subin_move + 1))
+
+
