2c2a50bbe6cc78c4af654257e4b59635e17d24b3
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-11-22 17:36:59 +0900

    solved 1697

--- DoIt_Python_Study/7weeks/bfs/1697.py
+++ DoIt_Python_Study/7weeks/bfs/1697.py
@@ -3,28 +3,34 @@ from collections import deque,defaultdict
 N, K = map(int,input().split())
 
 queue = deque([(N,0)])
-visited = defaultdict(bool)
+
+visited = [-1]*100001
+visited[N] = N
 
 while queue:
 
     subin_n, subin_move = queue.popleft()
 
     if subin_n == K:
+        path = []
+        while subin_n != N:
+            path.append(subin_n)
+            subin_n = visited[subin_n]
+        path.append(N)
         print(subin_move)
+        print(*path[::-1])
         break
 
-    if visited[subin_n]:
-        continue
-
-    visited[subin_n] = True
-
-    if 0 <= subin_n+1 <= 100000:
+    if 0 <= subin_n+1 <= 100000 and visited[subin_n+1] == -1:
         queue.append((subin_n+1,subin_move+1))
+        visited[subin_n+1] = subin_n
 
-    if 0 <= subin_n-1 <= 100000:
+    if 0 <= subin_n-1 <= 100000 and visited[subin_n-1] == -1:
         queue.append((subin_n-1, subin_move + 1))
+        visited[subin_n-1] = subin_n
 
-    if 0 <= 2*subin_n <= 100000:
+    if 0 <= 2*subin_n <= 100000 and visited[2*subin_n] == -1:
         queue.append((2*subin_n, subin_move + 1))
+        visited[2*subin_n] = subin_n
 
 
